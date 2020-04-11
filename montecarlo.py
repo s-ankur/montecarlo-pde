6eb2315f02aded25b9486627dbe6df36b1571311
ankur <s.ankursonawane@gmail.com>
2020-04-11 20:32:28 +0530

    Autopep

--- montecarlo.py
+++ montecarlo.py
@@ -6,17 +6,19 @@ import matplotlib.pyplot as plt
 import gif
 import numpy as np
 
-h=10e-2 # Distance between plates = 10cm
-lattice_points = 15 # Number of Points in lattice
-d=h/lattice_points  # Lattice size = 1cm
-laplace = False # False-> Poisson, True-> Laplace
-boundary_voltage_high = 5.0 # 5 Volts at Positive Plate
+h = 10e-2  # Distance between plates = 10cm
+lattice_points = 15  # Number of Points in lattice
+d = h / lattice_points  # Lattice size = 1cm
+laplace = False  # False-> Poisson, True-> Laplace
+boundary_voltage_high = 5.0  # 5 Volts at Positive Plate
 boundary_voltage_low = 0.0  # 0 Volts at Negative Plate
 epsilon_naught = 8.854e-12  # Permittivity of Vaccum
-charge_density = 2e-15 # Coulomb per meter cube
-N = 500 # Number of Random Walks
+charge_density = 2e-15  # Coulomb per meter cube
+N = 500  # Number of Random Walks
 
 # The Function \nabla^2(phi)  = f
+
+
 def f(x):
     if laplace:
         # For Laplace f = 0
@@ -25,30 +27,34 @@ def f(x):
         # For Poisson, assume that there is a constant charge density
         # between the plates
         # So f=rho/epsilon
-        return charge_density/epsilon_naught 
+        return charge_density / epsilon_naught
 
 # One Dimentional Boundary Conditions
+
+
 def g(x):
-    if x<=0:
+    if x <= 0:
         return boundary_voltage_low
     return boundary_voltage_high
 
 # Returns the Value of Potential Feild at a given point A with N random walks
-def poisson_approximation(A,N):
-    result =0
+
+
+def poisson_approximation(A, N):
+    result = 0
     for i in range(N):
-        x=A
-        F=0
+        x = A
+        F = 0
         while True:
-            if x<=0 or x>=h:
+            if x <= 0 or x >= h:
                 break
-            if random.randint(0,1):
-                x+=d
+            if random.randint(0, 1):
+                x += d
             else:
-                x-=d
-            F+=f(x)/h**2
-        result+=g(x)-F
-    result=result/N
+                x -= d
+            F += f(x) / h**2
+        result += g(x) - F
+    result = result / N
     return result
 
 # Function for plotting the potential
@@ -56,34 +62,33 @@ def poisson_approximation(A,N):
 def plot(x, y):
     plt.figure(figsize=(7, 5), dpi=100)
     plt.plot(x, y)
-    plt.text(h,0 , f'N = {N}',  ha='right')
-    plt.text(h,0.4 , f'L = {lattice_points}',  ha='right')
-    plt.ylim(-2,7)
+    plt.text(h, 0, f'N = {N}', ha='right')
+    plt.text(h, 0.4, f'L = {lattice_points}', ha='right')
+    plt.ylim(-2, 7)
     plt.xlabel('Distance from negative plate (Meters)')
     plt.ylabel('Potential (Volts)')
 #    plt.show()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     for laplace in True, False:
         # Analysis with respect to Number of Random Walks
-        frames=[]
-        lattice_points = 15 
-        for N in range(10,1500,20):
+        frames = []
+        lattice_points = 15
+        for N in range(10, 1500, 20):
             print(f"Calculating Monte Carlo with {lattice_points}x{lattice_points} lattice points and {N} random walks for {'Laplace' if laplace else 'Poisson'}")
-            lattice= np.linspace(0,h,num=lattice_points,endpoint=True)
-            frame=plot(lattice,[poisson_approximation(A, N) for A in lattice])
-            frames.append(frame)       
+            lattice = np.linspace(0, h, num=lattice_points, endpoint=True)
+            frame = plot(lattice, [poisson_approximation(A, N) for A in lattice])
+            frames.append(frame)
             gif.save(frames, f"{'Laplace' if laplace else 'Poisson'}_N.gif", duration=100)
 
         # Analysis with respect to number of Lattice Points
-        frames=[]
-        N=500
-        for lattice_points in range(2,41):
+        frames = []
+        N = 500
+        for lattice_points in range(2, 41):
             print(f"Calculating Monte Carlo with {lattice_points}x{lattice_points} lattice points and {N} random walks for {'Laplace' if laplace else 'Poisson'}")
-            d=h/lattice_points 
-            lattice= np.linspace(0,h,num=lattice_points,endpoint=True)
-            frame=plot(lattice,[poisson_approximation(A, N) for A in lattice])
-            frames.append(frame)       
+            d = h / lattice_points
+            lattice = np.linspace(0, h, num=lattice_points, endpoint=True)
+            frame = plot(lattice, [poisson_approximation(A, N) for A in lattice])
+            frames.append(frame)
             gif.save(frames, f"{'Laplace' if laplace else 'Poisson'}_Lattice.gif", duration=100)
-          
--- montecarlo2d.py
+++ montecarlo2d.py
@@ -7,17 +7,19 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 
-h=10e-2 # Distance between plates = 10cm
-lattice_points = 15 # Number of Points in lattice
-d=h/lattice_points  # Lattice size = 1cm
-laplace = True # False-> Poisson, True-> Laplace
-boundary_voltage_high = 5.0 # 5 Volts at Positive Plate
+h = 10e-2  # Distance between plates = 10cm
+lattice_points = 15  # Number of Points in lattice
+d = h / lattice_points  # Lattice size = 1cm
+laplace = True  # False-> Poisson, True-> Laplace
+boundary_voltage_high = 5.0  # 5 Volts at Positive Plate
 boundary_voltage_low = 0.0  # 0 Volts at Negative Plate
 epsilon_naught = 8.854e-12  # Permittivity of Vaccum
-charge_density = 6e-16 # Coulomb per meter cube
-N = 500 # Number of Random Walks
+charge_density = 6e-16  # Coulomb per meter cube
+N = 500  # Number of Random Walks
 
 # The Function \nabla^2(phi)  = f
+
+
 def f(x):
     if laplace:
         # For Laplace f = 0
@@ -26,56 +28,59 @@ def f(x):
         # For Poisson, assume that there is a constant charge density
         # between the plates
         # So f=rho/epsilon
-        return charge_density/epsilon_naught 
+        return charge_density / epsilon_naught
 
 # Two Dimentional Boundary Conditions
+
+
 def g(x):
-    if x[0]<=0:
+    if x[0] <= 0:
         return boundary_voltage_low
-    if x[0]>=h:
+    if x[0] >= h:
         return boundary_voltage_high
-    if x[1]<=0 or x[1]>=h:
+    if x[1] <= 0 or x[1] >= h:
         return boundary_voltage_low
 
 # Returns the Value of Potential Feild at a given point A with N random walks
 @np.vectorize
 def poisson_approximation(*A):
-    result =0
+    result = 0
     for i in range(N):
-        x=list(A)
-        F=0
+        x = list(A)
+        F = 0
         while True:
-            if x[0]<=0 or x[0]>=h or x[1]<=0 or x[1]>=h:
+            if x[0] <= 0 or x[0] >= h or x[1] <= 0 or x[1] >= h:
                 break
-            random_number = random.randint(0,3)
+            random_number = random.randint(0, 3)
             if random_number == 0:
-                x[0]+=d
+                x[0] += d
             elif random_number == 1:
-                x[0]-=d
+                x[0] -= d
             elif random_number == 2:
-                x[1]+=d
+                x[1] += d
             elif random_number == 3:
-                x[1]-=d
-            F+=f(x)/h**2
-        result+=g(x)-F
-    result=result/N
+                x[1] -= d
+            F += f(x) / h**2
+        result += g(x) - F
+    result = result / N
     return result
 
 # Function for plotting the potential
-def plot(x, y,z):
+
+
+def plot(x, y, z):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    ax.plot_surface(x,y, np.array(z),cmap=cm.jet, linewidth=0.1)
+    ax.plot_surface(x, y, np.array(z), cmap=cm.jet, linewidth=0.1)
     plt.xlabel('(Meters)')
     plt.ylabel('(Meters)')
     plt.show()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     for laplace in True, False:
         print(f"Calculating Monte Carlo with {lattice_points}x{lattice_points} lattice points and {N} random walks for {'Laplace' if laplace else 'Poisson'}")
-        lattice_x,lattice_y = np.mgrid[0:h:lattice_points*1j,0:h:lattice_points*1j]
-        z=poisson_approximation(lattice_x.ravel(),lattice_y.ravel()).reshape(lattice_x.shape)
-        plot(lattice_x,lattice_y,z)
-        
+        lattice_x, lattice_y = np.mgrid[0:h:lattice_points * 1j, 0:h:lattice_points * 1j]
+        z = poisson_approximation(lattice_x.ravel(), lattice_y.ravel()).reshape(lattice_x.shape)
+        plot(lattice_x, lattice_y, z)
